ec6b5e7dede5310013c4ad3ff3f19385cf79398e
TheAhmedAlaa <ahmedalaa251025@gmail.com>
2025-08-06 23:36:09 +0300

    Update wrapper.v

--- wrapper.v
+++ wrapper.v
@@ -1,12 +1,10 @@
-module top_module (clk, rst_n, MOSI, MISO, SS_n);
+module wrapper (clk, rst_n, MOSI, MISO, SS_n);
     input  clk, rst_n, MOSI, SS_n;
     output MISO;
-
     wire [9:0] rx_data;
     wire rx_valid;
     wire [7:0] tx_data;
     wire tx_valid;
-
-    SPI inst1 (clk, rst_n, SS_n, MOSI, tx_valid, tx_data, rx_valid, rx_data, MISO);
-    RAM inst2 (clk, rst_n, rx_data, rx_valid, tx_data, tx_valid);
+    SPI MASTERSLAVEE (MOSI,MISO,SS_n,rx_data,rx_valid,tx_data,tx_valid,clk,rst_n);
+    ram RAM (rx_data,rx_valid,tx_data,tx_valid,clk,rst_n);
 endmodule
